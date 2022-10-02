6cf5a681a007d903cf19bdd83b8e2a0943206916
colmanfranco <colmanfm@gmail.com>
2022-10-02 14:49:12 +0200

    Franco | Adds testing with jest, githuba actions and deployment folder to try build and deploy to vercel with terraform

---
+++ pages/index.page.tsx
@@ -0,0 +1,49 @@
+import type { NextPage } from 'next';
+import Head from 'next/head';
+import Image from 'next/image';
+import styles from '../styles/Home.module.css';
+import { getStoryblokApi, useStoryblokState } from "@storyblok/react";
+
+const Home = () => {
+  // const story = useStoryblokState(props.initialStory);
+  // if (!story.content) {
+  //   return <div>Loading...</div>;
+  // }
+  return (
+    <div className={styles.container}>
+      <Head>
+        <title>Franco Colman Mendoza | Software Engineer</title>
+      </Head>
+
+      <main>
+        <h1>
+        { 'My Site' }
+        </h1>
+      </main>
+
+      <footer>
+      </footer>
+    </div>
+  )
+}
+
+export default Home
+
+// export async function getStaticProps({ preview = false }) {
+//   const slug = "home";
+//   const sbParams = {
+//     version: "draft", // or 'published'
+//   };
+ 
+//   const storyblokApi = getStoryblokApi();
+//   const { data } = await storyblokApi.get(`cdn/stories/${slug}`, sbParams);
+ 
+//   return {
+//     props: {
+//       story: data ? data.story : false,
+//       key: data ? data.story.id : false,
+//       preview,
+//     },
+//     revalidate: 3600,
+//   };
+// }
