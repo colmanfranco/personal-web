c3bb591a2937d25d1c58a33c3beb849895a48eb4
colmanfranco <colmanfm@gmail.com>
2022-10-03 13:13:39 +0200

    Franco | Modifies vercel_project data

--- deployment/vercel.tf
+++ deployment/vercel.tf
@@ -8,20 +8,20 @@ resource "vercel_project" "personal_web" {
   }
 }
 
-data "vercel_project" "personal-web-project" {
-  name = "personal-web"
-}
+# data "vercel_project" "personal-web-project" {
+#   name = "personal-web"
+# }
 
 data "vercel_project_directory" "personal_web_directory" {
   path = "../../personal-web"
 }
 
-output "project_id" {
-  value = data.vercel_project.personal-web-project.id
-}
+# output "project_id" {
+#   value = data.vercel_project.personal-web-project.id
+# }
 
 resource "vercel_deployment" "personal_web_deployment" {
-  project_id  = data.vercel_project.personal-web-project.id
+  project_id  = vercel_project.personal_web.id
   files       = data.vercel_project_directory.personal_web_directory.files
   path_prefix = data.vercel_project_directory.personal_web_directory.path
   production  = true
