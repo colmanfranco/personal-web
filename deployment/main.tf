c47524c1112f614860fbec0138101622c4e13867
colmanfranco <colmanfm@gmail.com>
2022-10-03 12:59:06 +0200

    Franco | Moves resources to vercel terraform file

--- deployment/main.tf
+++ deployment/main.tf
@@ -7,10 +7,10 @@ terraform {
   }
 }
 
-provider "vercel" {
-  # Or omit this for the api_token to be read
-  # from the VERCEL_API_TOKEN environment variable
-  api_token = var.vercel_api_token
-}
+# provider "vercel" {
+#   # Or omit this for the api_token to be read
+#   # from the VERCEL_API_TOKEN environment variable
+#   api_token = var.vercel_api_token
+# }
 
 
