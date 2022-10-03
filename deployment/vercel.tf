2250b1b5114150de16d90b7fc60cac20ca591af0
Franco Colman Mendoza <colmanfm@gmail.com>
2022-10-03 16:54:14 +0200

    Franco | Removes vercel_project resource

--- deployment/vercel.tf
+++ deployment/vercel.tf
@@ -1,25 +1,29 @@
-resource "vercel_project" "personal_web" {
-  name      = "colmanfranco-personal-web"
-  framework = "nextjs"
-
-  git_repository = {
-    type = "github"
-    repo = "colmanfranco/personal-web"
-  }
+data "vercel_project" "personal_web" {
+  name = "colmanfranco-personal-web"
 }
+
+#resource "vercel_project" "personal_web" {
+#  name      = "colmanfranco-personal-web"
+#  framework = "nextjs"
+#
+#  git_repository = {
+#    type = "github"
+#    repo = "colmanfranco/personal-web"
+#  }
+#}
 
 data "vercel_project_directory" "personal_web_directory" {
   path = "../../personal-web"
 }
 
 resource "vercel_deployment" "personal_web_deployment" {
-  project_id  = vercel_project.personal_web.id
+  project_id  = data.vercel_project.personal_web.id
   files       = data.vercel_project_directory.personal_web_directory.files
   path_prefix = data.vercel_project_directory.personal_web_directory.path
   production  = true
 }
 
 resource "vercel_project_domain" "personal-web-domain" {
-  project_id = vercel_project.personal_web.id
+  project_id = data.vercel_project.personal_web.id
   domain     = "personal-web.colmanfranco.vercel.app"
 }
