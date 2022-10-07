3b3e531c61a770400acb702f417edf015fc9cdb1
Franco Colman Mendoza <colmanfm@gmail.com>
2022-10-07 09:13:06 +0200

    Franco | Adds empty tag to main page

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,13 +3,13 @@ import '../styles/globals.css'
 import type { AppProps } from 'next/app'
 import { storyblokInit, apiPlugin } from "@storyblok/react";
 
-const isPreview = isPreviewMode();
-const token = isPreview ? process.env.NEXT_PUBLIC_STORYBLOK_PREVIEW_KEY : process.env.NEXT_PUBLIC_STORYBLOK_KEY;
-
-storyblokInit({
-  accessToken: process.env.NEXT_PUBLIC_STORYBLOK_PREVIEW_TOKEN,
-  use: [apiPlugin],
-});
+// const isPreview = isPreviewMode();
+// const token = isPreview ? process.env.NEXT_PUBLIC_STORYBLOK_PREVIEW_KEY : process.env.NEXT_PUBLIC_STORYBLOK_KEY;
+//
+// storyblokInit({
+//   accessToken: process.env.NEXT_PUBLIC_STORYBLOK_PREVIEW_TOKEN,
+//   use: [apiPlugin],
+// });
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
