c2beced3af7df77bad134f995224d7c05839d5cd
colmanfranco <colmanfm@gmail.com>
2022-10-12 13:43:33 +0200

    Franco | Wrap main _app page with preview provider

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,6 +3,7 @@ import '../styles/globals.css'
 import type { AppProps } from 'next/app'
 import { storyblokInit, apiPlugin } from "@storyblok/react";
 import { isPreviewMode } from '../utils/isPreview';
+import { PreviewProvider } from '@providers/previewProvider';
 
 const isPreview = isPreviewMode();
 const token = isPreview ? process.env.NEXT_PUBLIC_STORYBLOK_PREVIEW_KEY : process.env.NEXT_PUBLIC_STORYBLOK_KEY;
@@ -14,13 +15,13 @@ storyblokInit({
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-      <>
-    <Head>
-      <title>Franco Colman Mendoza | Software Engineer</title>
-      <meta name="robots" content="noindex,nofollow" />
-    </Head>
-    <Component {...pageProps} />
-      </>
+    <PreviewProvider isPreview={isPreview}>
+      <Head>
+        <title>Franco Colman Mendoza | Software Engineer</title>
+        <meta name="robots" content="noindex,nofollow" />
+      </Head>
+      <Component {...pageProps} />
+    </PreviewProvider>
   );
 }
 
